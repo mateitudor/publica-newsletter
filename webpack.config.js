718888667101499cecf1900c5e5f3aa80a5adeff
Tudor Matei <mateitudor@gmail.com>
2025-04-09 13:17:08 +0300

    Global - Fix Webpack not creating the prod files

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,76 @@
 const path = require('path');
-const glob = require('glob');
+const fs = require('fs');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const HtmlMinimizerPlugin = require('html-minimizer-webpack-plugin');
 
-// Get the current directory name (e.g., "03_19")
-const currentDir = process.cwd().split('/').pop();
+// Get the directory where npm run build was executed
+const currentDir = process.env.INIT_CWD || process.cwd();
+console.log('Current directory:', currentDir);
 
-// Find all HTML files ending with -dev.html in the current directory
-const htmlFiles = glob.sync(`./${currentDir}/*-dev.html`);
+const allFiles = fs.readdirSync(currentDir);
+console.log('All files in directory:', allFiles);
+
+const inputFiles = allFiles.filter(file => file.endsWith('-dev.html'));
+console.log('Files matching *-dev.html:', inputFiles);
+
+if (inputFiles.length === 0) {
+  console.log('No files matching *-dev.html found in current directory');
+  process.exit(0);
+}
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
 
 module.exports = {
-  mode: 'production',
-  entry: htmlFiles.reduce((entries, file) => {
-    const name = path.basename(file, '.html');
-    entries[name] = file;
+  mode: isDevelopment ? 'development' : 'production',
+  entry: inputFiles.reduce((entries, file) => {
+    entries[file] = path.join(currentDir, file);
     return entries;
   }, {}),
   output: {
-    path: path.resolve(__dirname, currentDir),
-    filename: '[name].js',
+    path: currentDir,
+    filename: '[name].bundle.js',
   },
-  plugins: [
-    ...htmlFiles.map(file => {
-      const name = path.basename(file, '.html');
-      return new HtmlWebpackPlugin({
-        template: file,
-        filename: name.replace('-dev', '-prod') + '.html',
-        inject: false,
-        minify: {
-          removeComments: true,
-          collapseWhitespace: true,
-          removeRedundantAttributes: true,
-          useShortDoctype: true,
-          removeEmptyAttributes: true,
-          removeStyleLinkTypeAttributes: true,
-          keepClosingSlash: true,
-          minifyJS: true,
-          minifyCSS: true,
-          minifyURLs: true,
-        },
-      });
-    }),
-  ],
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new HtmlMinimizerPlugin(),
-    ],
+  devServer: {
+    static: {
+      directory: currentDir,
+      watch: {
+        ignored: /node_modules/,
+        usePolling: true,
+      },
+    },
+    port: 3000,
+    hot: true,
+    open: true,
+    watchFiles: ['*.html'],
+    devMiddleware: {
+      writeToDisk: true,
+    },
   },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: ['html-loader'],
+        exclude: /node_modules/,
+      }
+    ]
+  },
+  plugins: inputFiles.map(file => 
+    new HtmlWebpackPlugin({
+      template: path.join(currentDir, file),
+      filename: file.replace('-dev.html', '-prod.html'),
+      chunks: [file],
+      minify: !isDevelopment && {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true,
+      },
+    })
+  ),
 }; 
