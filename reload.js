5a35a4d171f528d85cb7d84199ec2e0a06559434
Tudor Matei <mateitudor@gmail.com>
2025-04-15 10:17:04 +0300

    Fixes webpack

---
+++ reload.js
@@ -0,0 +1,1 @@
+// dummy entry to enable live reload script injection
--- webpack.config.js
+++ webpack.config.js
@@ -2,75 +2,92 @@ const path = require('path');
 const fs = require('fs');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-// Get the directory where npm run build was executed
 const currentDir = process.env.INIT_CWD || process.cwd();
-console.log('Current directory:', currentDir);
-
 const allFiles = fs.readdirSync(currentDir);
-console.log('All files in directory:', allFiles);
-
 const inputFiles = allFiles.filter(file => file.endsWith('-dev.html'));
-console.log('Files matching *-dev.html:', inputFiles);
 
 if (inputFiles.length === 0) {
-  console.log('No files matching *-dev.html found in current directory');
-  process.exit(0);
+	console.log('No files matching *-dev.html found in current directory');
+	process.exit(0);
 }
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
 module.exports = {
-  mode: isDevelopment ? 'development' : 'production',
-  entry: inputFiles.reduce((entries, file) => {
-    entries[file] = path.join(currentDir, file);
-    return entries;
-  }, {}),
+	mode: isDevelopment ? 'development' : 'production',
+  entry: {
+    reload: path.resolve(__dirname, 'reload.js'),
+  }, // no bundle.js
   output: {
     path: currentDir,
-    filename: '[name].bundle.js',
+    publicPath: '/',
+    filename: '[name].js',
   },
   devServer: {
     static: {
-      directory: currentDir,
-      watch: {
-        ignored: /node_modules/,
-        usePolling: true,
-      },
+      directory: path.resolve(__dirname, 'nothing-to-serve'),
+      watch: false,
     },
     port: 3000,
-    hot: true,
-    open: true,
-    watchFiles: ['*.html'],
+    open: {
+      target: inputFiles[0].replace('-dev.html', '-prod.html'),
+    },
+    hot: false,
+    liveReload: true,
     devMiddleware: {
-      writeToDisk: true,
+      writeToDisk: (filePath) => {
+        return filePath.endsWith('.html');
+      },
+    },
+    watchFiles: {
+      paths: ['**/*.html', '**/*.jpg', '**/*.jpeg', '**/*.png', '**/*.gif', '**/*.svg'],
+      options: {
+        usePolling: true,
+        ignored: /node_modules/,
+      },
     },
   },
-  module: {
-    rules: [
-      {
-        test: /\.html$/,
-        use: ['html-loader'],
-        exclude: /node_modules/,
+	module: {
+		rules: [
+			{
+				test: /\.html$/,
+				use: ['html-loader'],
+				exclude: /node_modules/,
+			}
+		]
+	},
+  plugins: [
+    ...inputFiles.map(file =>
+      new HtmlWebpackPlugin({
+        template: path.join(currentDir, file),
+        filename: file.replace('-dev.html', '-prod.html'),
+        inject: true,
+        chunks: ['reload'],
+        minify: {
+          removeComments: true,
+          collapseWhitespace: true,
+          removeRedundantAttributes: true,
+          useShortDoctype: true,
+          removeEmptyAttributes: true,
+          removeStyleLinkTypeAttributes: true,
+          keepClosingSlash: true,
+          minifyJS: true,
+          minifyCSS: true,
+          minifyURLs: true,
+        },
+      })
+    ),
+    {
+      apply: (compiler) => {
+        compiler.hooks.afterCompile.tap('WatchDevHTMLPlugin', (compilation) => {
+          inputFiles.forEach(file => {
+            const fullPath = path.join(currentDir, file);
+            compilation.fileDependencies.add(fullPath);
+            console.log('[WatchDevHTMLPlugin] Watching:', fullPath);
+          });
+        });
       }
-    ]
-  },
-  plugins: inputFiles.map(file => 
-    new HtmlWebpackPlugin({
-      template: path.join(currentDir, file),
-      filename: file.replace('-dev.html', '-prod.html'),
-      chunks: [file],
-      minify: !isDevelopment && {
-        removeComments: true,
-        collapseWhitespace: true,
-        removeRedundantAttributes: true,
-        useShortDoctype: true,
-        removeEmptyAttributes: true,
-        removeStyleLinkTypeAttributes: true,
-        keepClosingSlash: true,
-        minifyJS: true,
-        minifyCSS: true,
-        minifyURLs: true,
-      },
-    })
-  ),
-}; 
+    }
+    
+  ]  
+};
